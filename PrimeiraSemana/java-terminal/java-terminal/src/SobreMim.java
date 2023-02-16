42f969718d55c8fb8b1b9dd2e733b16cbb087ba9
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-02-16 10:26:37 -0300

    Utilizando Scanner

--- PrimeiraSemana/java-terminal/java-terminal/src/SobreMim.java
+++ PrimeiraSemana/java-terminal/java-terminal/src/SobreMim.java
@@ -1,15 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class SobreMim {
 
     public static void main(String[] args) {
     
-        String nome = args[0];
-        String sobreNome = args[1];
-        int idade = Integer.valueOf(args[3]);
-        double altura = Double.valueOf(args[4]);
+        Scanner scanner  = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Qual é o seu nome? ");
+        String nome = scanner.next();
+
+        System.out.println("Qual seu segundo nome? ");
+        String sobreNome = scanner.next();
+
+        System.out.println("Qual sua idade? ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite seu altura: ");
+        Double altura = scanner.nextDouble();
+        System.out.println();
 
         System.out.println("Olá, me chamo " + nome + " " + sobreNome);
         System.out.println("Tenho " + idade + " anos");
-        System.out.println("Minha altura é " + altura + "Cm ");
+        System.out.println("Minha altura é " + altura + " Cm ");
 
     }
 }
