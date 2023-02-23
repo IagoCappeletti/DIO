451514728229b681a9d94280e7b4e8495a92172e
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-02-22 23:31:28 -0300

    Adicionei comentários

--- PrimeiraSemana/arrays/Ex2_Consoantes.java
+++ PrimeiraSemana/arrays/Ex2_Consoantes.java
@@ -1,3 +1,9 @@
+/*
+    Fazer um Programa que irá ler um vetor de 6 caracteres,
+    e mostrar quantas consoantes forem lidas.
+    imprimir as consoantes.
+ */
+
 import java.util.Scanner;
 
 public class Ex2_Consoantes {
@@ -16,15 +22,16 @@ public class Ex2_Consoantes {
                         letra.equalsIgnoreCase("i") |
                         letra.equalsIgnoreCase("o") |
                         letra.equalsIgnoreCase("u") )) {
+                        //atribua a letra ao elemento do vetor na posição indicada pelo contador, caso seja consoante.
                         consoantes[i] = letra;
                         quantidadeConsoantes++;
                         }
             }
 
             System.out.print("Consoantes: ");
-            for (String consoante : consoantes) {
-                if (consoante != null){
-                    System.out.print(consoante + " ");    
+            for (String consoante : consoantes) { //para cada consoante dentro do array consoantes
+                if (consoante != null){ //se o valor da consoante for diferente de null
+                    System.out.print(consoante + " "); //imprima a consoante
                 }
             }
 
